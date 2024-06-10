720c665e67a3cb5daf1a3534770609c703dd7e92
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-06-10 23:43:59 +0530

    auth from app removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,8 @@
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import { app } from "./firebase";
 import "./App.css";
 import Signup from "./pages/Signup";
 
-const auth = getAuth(app);
-
 function App() {
-  const signupUser = () => {
-    // createUserWithEmailAndPassword(auth, email, password)
-    createUserWithEmailAndPassword(
-      auth,
-      "thoufiqizhar022@gmail.com",
-      "THOUFIQizhar@022"
-    ).then((value) => console.log(value));
-  };
-
   return (
     <div className="App">
       <Signup />
