f5d70dd522f6f26e3181b490db2d7d19aaf9db96
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-06-10 23:30:32 +0530

    Learning Authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import { getDatabase, ref, set } from "firebase/database";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import { app } from "./firebase";
-import './App.css'
+import "./App.css";
+import Signup from "./pages/Signup";
 
-const db = getDatabase(app)
+const auth = getAuth(app);
 
 function App() {
-   const putData = () => {
-    // set(ref(database, path), {object})
-    set(ref(db, "users/thoufiq"), {
-      id: 1,
-      name: "Thoufiq",
-      age: 21,
-    })
-   }  
+  const signupUser = () => {
+    // createUserWithEmailAndPassword(auth, email, password)
+    createUserWithEmailAndPassword(
+      auth,
+      "thoufiqizhar022@gmail.com",
+      "THOUFIQizhar@022"
+    ).then((value) => console.log(value));
+  };
 
   return (
     <div className="App">
-      <h1>Firebase React App</h1>
-      <button onClick={putData}>Put Data</button>
+      <Signup />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
