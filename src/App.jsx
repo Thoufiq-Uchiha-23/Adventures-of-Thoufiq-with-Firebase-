b77eaa7241554624f32ad468dac83a19c33cf170
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-06-11 23:46:14 +0530

    Sign in added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { app } from "./firebase";
 import "./App.css";
 import Signup from "./pages/Signup";
+import SigninPage from "./pages/Signin";
 
 function App() {
   return (
     <div className="App">
       <Signup />
+      <SigninPage />
     </div>
   );
 }
