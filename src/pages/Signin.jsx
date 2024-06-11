132909703a9c4f3cdffce9658e4b4734487a7193
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-06-11 23:39:56 +0530

    Structure set

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
 
 const SigninPage = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
     return (
         <div>
+            <h1>Signin Page</h1>
             <label>Enter your email</label>
-            <input type="email" placeholder="Enter your email here" />
+            <input type="email" onChange={(e) => setEmail(e.target.value)} value={email} placeholder="Enter your email here" />
             <label>Enter your password</label>
-            <input type="password" placeholder="Enter your password here" />
+            <input type="password" onChange={(e) => setPassword(e.target.value)} value={password} placeholder="Enter your password here" />
             <button>Sign me in</button>
         </div>
     )
