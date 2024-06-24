e623e71e0a9ec93848ac9207c54ba9910756d4a7
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-06-24 23:40:58 +0530

    prettier

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -9,7 +9,9 @@ const Signup = () => {
   const [password, setPassword] = useState("");
 
   const createUser = () => {
-    createUserWithEmailAndPassword(auth, email, password).then((value) => alert("Success"));
+    createUserWithEmailAndPassword(auth, email, password).then((value) =>
+      alert("Success")
+    );
     // Success
   };
 
@@ -17,15 +19,19 @@ const Signup = () => {
     <div className="signup-page">
       <h1>Sign Up Page</h1>
       <label>Email</label>
-      <input 
-      onChange={(e) => setEmail(e.target.value)} 
-      value={email}
-      type="email" placeholder="Enter your email here" />
+      <input
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+        type="email"
+        placeholder="Enter your email here"
+      />
       <label>Password</label>
-      <input 
-      onChange={(e) => setPassword(e.target.value)} 
-      value={password}
-      type="password" placeholder="Enter your password here" />
+      <input
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+        type="password"
+        placeholder="Enter your password here"
+      />
       <button onClick={createUser}>Sign Up</button>
     </div>
   );
