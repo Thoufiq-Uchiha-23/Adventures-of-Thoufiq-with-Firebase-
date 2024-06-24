a701ee5334304a58ffa61c13128c367708273b2b
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-06-24 23:40:38 +0530

    prettier

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,29 +1,40 @@
-import React, {useState} from "react";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
-import { app } from "../firebase"
+import React, { useState } from "react";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { app } from "../firebase";
 
-const auth = getAuth(app)
+const auth = getAuth(app);
 
 const SigninPage = () => {
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const signinUser = () => {
-        signInWithEmailAndPassword(auth, email, password).then((value) => console.log("Signin success"))
-        .catch(err =>  console.log(err))
-    }
+  const signinUser = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((value) => console.log("Signin success"))
+      .catch((err) => console.log(err));
+  };
 
-    return (
-        <div>
-            {/* Sign in Page Authentication */}
-            <h1>Signin Page</h1>
-            <label>Enter your email</label>
-            <input type="email" onChange={(e) => setEmail(e.target.value)} value={email} placeholder="Enter your email here" />
-            <label>Enter your password</label>
-            <input type="password" onChange={(e) => setPassword(e.target.value)} value={password} placeholder="Enter your password here" />
-            <button onClick={signinUser}>Sign me in</button>
-        </div>
-    )
-}
+  return (
+    <div>
+      {/* Sign in Page Authentication */}
+      <h1>Signin Page</h1>
+      <label>Enter your email</label>
+      <input
+        type="email"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+        placeholder="Enter your email here"
+      />
+      <label>Enter your password</label>
+      <input
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+        placeholder="Enter your password here"
+      />
+      <button onClick={signinUser}>Sign me in</button>
+    </div>
+  );
+};
 
-export default SigninPage
+export default SigninPage;
