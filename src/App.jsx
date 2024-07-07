e93e7c75c7edece0af9503928886895a652f4772
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-07-07 18:20:49 +0530

    learnt Sign up and login with google authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,42 @@
 import { app } from "./firebase";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Signup from "./pages/Signup";
 import SigninPage from "./pages/Signin";
+import { onAuthStateChanged, signOut, getAuth } from "firebase/auth";
+
+const auth = getAuth(app);
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // Yes, you are logged in
+        setUser(user);
+      } else {
+        // User is logged out
+        console.log("You are logged out");
+        setUser(null);
+      }
+    });
+  }, []);
+
+  if (user === null) {
+    return (
+      <div className="App">
+        <Signup />
+        <SigninPage />
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <Signup />
-      <SigninPage />
+      <h1>Hello {user.email}</h1>
+      <button onClick={() => signOut(auth)}>Logout</button>
     </div>
   );
 }
-
 export default App;
